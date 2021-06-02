bb8f3e64ea2410bceed392c98c8dc204e2779db1
Tomaz <tomaz@celtra.com>
2021-06-02 11:56:24 +0200

    x3

--- src/page.ts
+++ src/page.ts
@@ -155,7 +155,7 @@ export const getPostContent = async (page: Page): Promise<Partial<FbPost>> => {
 export const getVideoUrl = async (page: Page): Promise<string|null> => {
     await page.waitForSelector('.widePic > div > div');
     // This is needed because for some reason the video sometimes does not load if we click too fast
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(3000);
     const playClicked = await page.$eval('#viewport', async (el): Promise<boolean> => {
         const firstPlayButton = el.querySelector<HTMLDivElement>('.widePic > div > div');
         if (firstPlayButton) {
@@ -164,7 +164,7 @@ export const getVideoUrl = async (page: Page): Promise<string|null> => {
         }
         return false;
     });
-    //await page.waitForTimeout(2000);
+
     if (playClicked) {
         log.debug('Clicked play...');
         await page.waitForSelector('video', {timeout: 3000});
